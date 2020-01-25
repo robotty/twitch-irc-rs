4156e53f6903f61e95509c12b0accba2fee12ad1
Ruben Anders <ruben.anders@robotty.de>
2020-01-25 17:16:44 +0100

    Working snapshot

--- src/client/transport/mod.rs
+++ src/client/transport/mod.rs
@@ -1,22 +1,34 @@
-mod message_stream;
-mod transport_error;
-
 use crate::message::AsRawIRC;
 use crate::message::IRCMessage;
+use crate::message::IRCParseError;
+use async_tungstenite::tokio::connect_async;
 use bytes::Bytes;
 use futures::future::ready;
 use futures::prelude::*;
-use futures::prelude::*;
-pub use message_stream::MessageStream;
+use futures::stream::TryStreamExt;
+use std::convert::From;
+use thiserror::Error;
 use tokio::io::BufReader;
 use tokio::net::TcpStream;
 use tokio::prelude::*;
 use tokio_util::codec::{BytesCodec, FramedWrite};
-pub use transport_error::TransportError;
+use tungstenite::Error as WSError;
+use tungstenite::Message as WSMessage;
+use url::Url;
+
+#[derive(Debug, Error)]
+pub enum TransportError {
+    #[error("{0}")]
+    IOError(#[from] std::io::Error),
+    #[error("{0}")]
+    WebSocketError(#[from] WSError),
+    #[error("{0}")]
+    IRCParseError(#[from] IRCParseError),
+}
 
 pub struct Transport {
     pub incoming_messages: Box<dyn Stream<Item = Result<IRCMessage, TransportError>>>,
-    pub outgoing_messages: Box<dyn Sink<IRCMessage, Error = std::io::Error>>,
+    pub outgoing_messages: Box<dyn Sink<IRCMessage, Error = TransportError>>,
 }
 
 async fn new_tcp() -> std::io::Result<Transport> {
@@ -24,10 +36,46 @@ async fn new_tcp() -> std::io::Result<Transport> {
 
     let (read_half, write_half) = tokio::io::split(socket);
 
-    let message_stream = MessageStream::new(BufReader::new(read_half).lines());
+    let message_stream = BufReader::new(read_half)
+        .lines()
+        .map_err(TransportError::from)
+        .and_then(|s| ready(IRCMessage::parse(&s).map_err(TransportError::from)));
     let message_sink = FramedWrite::new(write_half, BytesCodec::new())
-        .with(|str: String| ready(Ok::<Bytes, std::io::Error>(Bytes::from(str))))
-        .with(|msg: IRCMessage| ready(Ok::<String, std::io::Error>(msg.as_raw_irc())));
+        .with(|s: String| ready(Ok::<Bytes, TransportError>(Bytes::from(s))))
+        .with(|msg: IRCMessage| {
+            let mut s = msg.as_raw_irc();
+            s.push_str("\r\n");
+            ready(Ok::<String, TransportError>(s))
+        });
+
+    Ok(Transport {
+        incoming_messages: Box::new(message_stream),
+        outgoing_messages: Box::new(message_sink),
+    })
+}
+
+async fn new_ws() -> Result<Transport, tungstenite::error::Error> {
+    let (ws_stream, _response) =
+        connect_async(Url::parse("wss://irc-ws.chat.twitch.tv").unwrap()).await?;
+
+    let (write_half, read_half) = futures::stream::StreamExt::split(ws_stream);
+
+    let message_stream = read_half
+        .map_err(TransportError::from)
+        .try_filter_map(|ws_message| {
+            ready(Ok::<_, TransportError>(match ws_message {
+                WSMessage::Text(text) => Some(futures::stream::iter(
+                    text.lines().map(|s| s.to_owned()).map(Ok),
+                )),
+                _ => None,
+            }))
+        })
+        .try_flatten()
+        .and_then(|s| ready(IRCMessage::parse(&s).map_err(TransportError::from)));
+
+    let message_sink = write_half
+        .with(|str: String| ready(Ok::<WSMessage, TransportError>(WSMessage::Text(str))))
+        .with(|msg: IRCMessage| ready(Ok::<String, TransportError>(msg.as_raw_irc())));
 
     Ok(Transport {
         incoming_messages: Box::new(message_stream),
