c80123db46ad29b4d7975634daa78d5c990976f9
Ruben Anders <ruben.anders@robotty.de>
2026-03-30 23:09:06 +0200

    Document changes to native-roots in README. (#223)

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,8 @@
 //!
 //! # Getting started
 //!
-//! The central feature of this library is the `TwitchIRCClient` which connects to Twitch IRC
-//! for you using a pool of connections and handles all the important bits. Here is a minimal
-//! example to get you started:
+//! The central feature of this library is the `TwitchIRCClient` which connects to Twitch IRC for you using a pool of
+//! connections and handles all the important bits. Here is a minimal example to get you started:
 //!
 //! ```no_run
 //! use twitch_irc::login::StaticLoginCredentials;
@@ -47,13 +46,12 @@
 //! # Features
 //!
 //! * Simple API
-//! * Integrated connection pool, new connections will be made based on your application's demand
-//!   (based on amount of channels joined as well as number of outgoing messages)
+//! * Integrated connection pool, new connections will be made based on your application's demand (based on amount of
+//!   channels joined as well as number of outgoing messages)
 //! * Automatic reconnect of failed connections, automatically re-joins channels
 //! * Rate limiting of new connections
 //! * Support for refreshing login tokens, see below
-//! * Fully parses all message types (see [`ServerMessage`](message/enum.ServerMessage.html)
-//!   for all supported types)
+//! * Fully parses all message types (see [`ServerMessage`](message/enum.ServerMessage.html) for all supported types)
 //! * Can connect using all protocol types supported by Twitch
 //! * Supports Rustls as well as Native TLS
 //! * No unsafe code
@@ -79,13 +77,12 @@
 //!
 //! The `TwitchIRCClient` handle can also be cloned and then used from multiple threads.
 //!
-//! See the documentation on [`TwitchIRCClient`](struct.TwitchIRCClient.html)
-//! for the possible methods.
+//! See the documentation on [`TwitchIRCClient`](struct.TwitchIRCClient.html) for the possible methods.
 //!
 //! # Receive and handle messages
 //!
-//! Incoming messages are [`ServerMessage`](message/enum.ServerMessage.html)s. You can use a match
-//! block to differentiate between the possible server messages:
+//! Incoming messages are [`ServerMessage`](message/enum.ServerMessage.html)s. You can use a match block to
+//! differentiate between the possible server messages:
 //!
 //! ```no_run
 //! # use twitch_irc::message::ServerMessage;
@@ -110,8 +107,8 @@
 //!
 //! # Logging in
 //!
-//! `twitch_irc` ships with [`StaticLoginCredentials`](login/struct.StaticLoginCredentials.html)
-//! and [`RefreshingLoginCredentials`](login/struct.RefreshingLoginCredentials.html).
+//! `twitch_irc` ships with [`StaticLoginCredentials`](login/struct.StaticLoginCredentials.html) and
+//! [`RefreshingLoginCredentials`](login/struct.RefreshingLoginCredentials.html).
 //!
 //! For simple cases, `StaticLoginCredentials` fulfills all needs:
 //!
@@ -127,11 +124,9 @@
 //! );
 //! ```
 //!
-//! However for most applications it is strongly recommended to have your login token automatically
-//! refreshed when it expires. For this, enable one of the `refreshing-token` feature flags
-//! (see [Feature flags](#feature-flags)), and use
-//! [`RefreshingLoginCredentials`](login/struct.RefreshingLoginCredentials.html), for example
-//! like this:
+//! However for most applications it is strongly recommended to have your login token automatically refreshed when it
+//! expires. For this, enable one of the `refreshing-token` feature flags (see [Feature flags](#feature-flags)), and use
+//! [`RefreshingLoginCredentials`](login/struct.RefreshingLoginCredentials.html), for example like this:
 //!
 //! ```no_run
 //! # #[cfg(feature = "__refreshing-token")] {
@@ -181,66 +176,59 @@
 //! # }
 //! ```
 //!
-//! `RefreshingLoginCredentials` needs an implementation of `TokenStorage` that depends
-//! on your application, to retrieve the token or update it. For example, you might put the token
-//! in a config file you overwrite, some extra file for secrets, or a database.
+//! `RefreshingLoginCredentials` needs an implementation of `TokenStorage` that depends on your application, to retrieve
+//! the token or update it. For example, you might put the token in a config file you overwrite, some extra file for
+//! secrets, or a database.
 //!
-//! In order to get started with `RefreshingLoginCredentials`, you need to have initial access
-//! and refresh tokens present in your storage. You can fetch these tokens using the
-//! [OAuth authorization code flow](https://dev.twitch.tv/docs/authentication/getting-tokens-oauth#oauth-authorization-code-flow).
-//! There is also a [`GetAccessTokenResponse`](crate::login::GetAccessTokenResponse) helper struct
-//! that allows you to decode the `POST /oauth2/token` response as part of the authorization process.
-//! See the documentation on that type for details on usage and how to convert the decoded response
-//! to a `UserAccessToken` that you can then write to your `TokenStorage`.
+//! In order to get started with `RefreshingLoginCredentials`, you need to have initial access and refresh tokens
+//! present in your storage. You can fetch these tokens using the [OAuth authorization code
+//! flow](https://dev.twitch.tv/docs/authentication/getting-tokens-oauth#oauth-authorization-code-flow). There is also a
+//! [`GetAccessTokenResponse`](crate::login::GetAccessTokenResponse) helper struct that allows you to decode the `POST
+//! /oauth2/token` response as part of the authorization process. See the documentation on that type for details on
+//! usage and how to convert the decoded response to a `UserAccessToken` that you can then write to your `TokenStorage`.
 //!
 //! # Close the client
 //!
-//! To close the client, drop all clones of the `TwitchIRCClient` handle. The client will shut down
-//! and end the stream of incoming messages once all processing is done.
+//! To close the client, drop all clones of the `TwitchIRCClient` handle. The client will shut down and end the stream
+//! of incoming messages once all processing is done.
 //!
 //! # Feature flags
 //!
 //! This library has these optional feature toggles:
-//! * **`transport-tcp`** enables `PlainTCPTransport`, to connect using a plain-text TLS socket
-//!   using the normal IRC protocol.
-//!     * `transport-tcp-native-tls` enables `SecureTCPTransport` which will then use OS-native
-//!       TLS functionality to make a secure connection. Root certificates are the ones configured
-//!       in the operating system.
-//!     * `transport-tcp-rustls-native-roots` enables `SecureTCPTransport` using [Rustls][rustls]
-//!       as the TLS implementation, but will use the root certificates configured in the
-//!       operating system.
-//!     * `transport-tcp-rustls-webpki-roots` enables `SecureTCPTransport` using [Rustls][rustls]
-//!       as the TLS implementation, and will statically embed the current
-//!       [Mozilla root certificates][mozilla-roots] as the trusted root certificates.
-//! * **`transport-ws`** enables `PlainWSTransport` to connect using the Twitch-specific websocket
-//!   method. (Plain-text)
-//!     * `transport-ws-native-tls` further enables `SecureWSTransport` which will then use OS-native
-//!       TLS functionality to make a secure connection. Root certificates are the ones configured
-//!       in the operating system.
-//!     * `transport-ws-rustls-native-roots` enables `SecureWSTransport` using [Rustls][rustls]
-//!       as the TLS implementation, but will use the root certificates configured in the
-//!       operating system.
-//!     * `transport-ws-rustls-webpki-roots` enables `SecureWSTransport` using [Rustls][rustls]
-//!       as the TLS implementation, and will statically embed the current
-//!       [Mozilla root certificates][mozilla-roots] as the trusted root certificates.
+//! * **`transport-tcp`** enables `PlainTCPTransport`, to connect using a plain-text TLS socket using the normal IRC
+//!   protocol.
+//!     * `transport-tcp-native-tls` enables `SecureTCPTransport` which will then use OS-native TLS functionality to
+//!       make a secure connection. Root certificates are the ones configured in the operating system.
+//!     * `transport-tcp-rustls-native-roots` enables `SecureTCPTransport` using [Rustls][rustls] as the TLS
+//!       implementation, will use the root certificates configured in the operating system via
+//!       `rustls-platform-verifier`.
+//!     * `transport-tcp-rustls-webpki-roots` enables `SecureTCPTransport` using [Rustls][rustls] as the TLS
+//!       implementation, and will statically embed the current [Mozilla root certificates][mozilla-roots] as the
+//!       trusted root certificates.
+//! * **`transport-ws`** enables `PlainWSTransport` to connect using the Twitch-specific websocket method. (Plain-text)
+//!     * `transport-ws-native-tls` further enables `SecureWSTransport` which will then use OS-native TLS functionality
+//!       to make a secure connection. Root certificates are the ones configured in the operating system.
+//!     * `transport-ws-rustls-native-roots` enables `SecureWSTransport` using [Rustls][rustls] as the TLS
+//!       implementation, but will use the root certificates configured in the operating system via
+//!       `rustls-platform-verifier`.
+//!     * `transport-ws-rustls-webpki-roots` enables `SecureWSTransport` using [Rustls][rustls] as the TLS
+//!       implementation, and will statically embed the current [Mozilla root certificates][mozilla-roots] as the
+//!       trusted root certificates.
 //! * Three different feature flags are provided to enable the
 //!   [`RefreshingLoginCredentials`](crate::login::RefreshingLoginCredentials):
-//!     * `refreshing-token-native-tls` enables this feature using the OS-native TLS functionality
-//!       to make secure connections. Root certificates are the ones configured
-//!       in the operating system.
-//!     * `refreshing-token-rustls-native-roots` enables this feature using
-//!       [Rustls][rustls] as the TLS implementation, but will use the root certificates configured
-//!       in the operating system.
-//!     * `refreshing-token-rustls-webpki-roots` enables this feature using [Rustls][rustls]
-//!       as the TLS implementation, and will statically embed the current [Mozilla root
-//!       certificates][mozilla-roots] as the trusted root certificates.
-//! * **`metrics-collection`** enables a set of metrics to be exported from the client. See the
-//!   documentation on [`ClientConfig::metrics_config`] for details. You may also want to see the `metrics` example,
-//!   which also contains further helpful guidelines on a possible setup to make use of this feature,
-//!   including a [Grafana dashboard template](https://grafana.com/grafana/dashboards/20702).
-//! * **`with-serde`** pulls in `serde` v1.0 and adds `#[derive(Serialize, Deserialize)]` to many
-//!   structs. This feature flag is automatically enabled when using any of the `refreshing-token`
-//!   feature flags.
+//!     * `refreshing-token-native-tls` enables this feature using the OS-native TLS functionality to make secure
+//!       connections. Root certificates are the ones configured in the operating system.
+//!     * `refreshing-token-rustls-native-roots` enables this feature using [Rustls][rustls] as the TLS implementation,
+//!       but will use the root certificates configured in the operating system via `rustls-platform-verifier`.
+//!     * `refreshing-token-rustls-webpki-roots` enables this feature using [Rustls][rustls] as the TLS implementation,
+//!       and will statically embed the current [Mozilla root certificates][mozilla-roots] as the trusted root
+//!       certificates.
+//! * **`metrics-collection`** enables a set of metrics to be exported from the client. See the documentation on
+//!   [`ClientConfig::metrics_config`] for details. You may also want to see the `metrics` example, which also contains
+//!   further helpful guidelines on a possible setup to make use of this feature, including a [Grafana dashboard
+//!   template](https://grafana.com/grafana/dashboards/20702).
+//! * **`with-serde`** pulls in `serde` v1.0 and adds `#[derive(Serialize, Deserialize)]` to many structs. This feature
+//!   flag is automatically enabled when using any of the `refreshing-token` feature flags.
 //!
 //! By default, `transport-tcp` and `transport-tcp-native-tls` are enabled.
 //!
